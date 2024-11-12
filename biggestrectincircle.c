531bb0b07189bff4b0a4ab0be8109adbd3f2bdab
Abhinav Singh <abhinavsingh7529@gmail.com>
2024-11-12 12:54:51 +0530

    Add files via upload

---
+++ biggestrectincircle.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    float radius=5.3, area;
+    area = 2*radius*radius;
+    printf("The area of the largest rectangle that can be inscribed in the circle is: %.2f\n", area);
+    return 0;
+}
